18438a8527f741caad987d0a41270564b5669665
sochetanie <allagorik2901@gmail.com>
2017-04-28 10:44:50 -0400

    Hero

--- spec/count_sentences_spec.rb
+++ spec/count_sentences_spec.rb
@@ -44,6 +44,7 @@ describe String do
 
     it "returns the number of sentences in a complex string" do
       complex_string = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+      # binding.pry
       expect(complex_string.count_sentences).to eq(4)
     end
   end
